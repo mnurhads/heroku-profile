b679e9fc1e77900795860e50a526dee464597e22
mnurhad <nurhadi@nominavipindonesia.com>
2022-10-18 13:44:26 +0700

    first commit

---
+++ web/index.php
@@ -0,0 +1,61 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
+// new route test
+$app->get('/cow', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+// database
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+          array(
+          'pdo.server' => array(
+              'driver'   => 'pgsql',
+              'user' => $dbopts["user"],
+              'password' => $dbopts["pass"],
+              'host' => $dbopts["host"],
+              'port' => $dbopts["port"],
+              'dbname' => ltrim($dbopts["path"],'/')
+              )
+          )
+);
+
+$app->get('/customer', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT * FROM customer');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
+$app->run();
